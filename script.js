93c0cc1044a9aa4108762cc7345b9a7b16c31bdd
HenriqueFilinto <filintotwt@gmail.com>
2026-02-19 17:37:12 -0300

    changed theme/colors and background

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const audio = document.getElementById('rainOnBrick');
 const icon = document.getElementById('icon');
+const video = document.getElementById('bg-video');
 
+video.playbackRate = 0.7;
 audio.volume = 0.1;
 
 document.body.addEventListener('click', () => {
